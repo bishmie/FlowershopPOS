439e2f2bcfc01569e032e96f95b72274ce54b2fe
bishmie <bishmimalshi@gmail.com>
2024-10-25 13:49:30 +0530

    add script

---
+++ script/script.js
@@ -0,0 +1,14 @@
+function loadPage(event, page) {
+    event.preventDefault(); 
+    const contentArea = document.getElementById("content");
+  
+    fetch(page)
+      .then(response => response.text())
+      .then(data => {
+        contentArea.innerHTML = data; 
+      })
+      .catch(error => {
+        console.error("Error loading page:", error);
+        contentArea.innerHTML = "<p>Failed to load page. Please try again later.</p>";
+      });
+  }
